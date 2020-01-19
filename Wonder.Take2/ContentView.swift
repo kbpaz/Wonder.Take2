7128cdc57bf9ab9207ac824b68c3e23011ba9da0
Katerin Paz <kbpaz@bhcc.edu>
2020-01-19 12:14:54 -0500

    Initial Commit

---
+++ Wonder.Take2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wonder.Take2
+//
+//  Created by Katerin Paz on 1/19/20.
+//  Copyright © 2020 Katerin Paz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wonder.Take2Tests/Wonder_Take2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Wonder_Take2Tests.swift
+//  Wonder.Take2Tests
+//
+//  Created by Katerin Paz on 1/19/20.
+//  Copyright © 2020 Katerin Paz. All rights reserved.
+//
+
+import XCTest
+@testable import Wonder_Take2
+
+class Wonder_Take2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
